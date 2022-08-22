9a7e9b6cb680f40562e1597efb3f4125740fd55f
deepak-singh19 <deepak.singh.16csc@bml.edu.in>
2022-08-22 12:41:27 +0530

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import Feature from "./feature";
+import Student from "./student"
+import User from "./Components/user"
+
+
+function App() {
+  return (
+    <>
+    {/* <Feature/> */}
+    
+    {/* <Student/> */}
+    <User/>
+  </>
+  );
+}
+
+export default App;
